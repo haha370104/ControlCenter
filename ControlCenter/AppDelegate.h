591280b815858bee2bb85ab1bb2386b29a66dcde
haha370104 <137368440@qq.com>
2016-07-28 13:45:47 +0800

    Initial Commit

---
+++ ControlCenter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ControlCenter
+//
+//  Created by 罗向宇 on 16/7/28.
+//  Copyright © 2016年 罗向宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
